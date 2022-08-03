52f8996737b5b50e54efc38be43a5e91569fbdf2
Gonzalez75 <guilherme75gonzalez@gmail.com>
2022-08-03 11:55:12 -0300

    commit #2

---
+++ form/DadosBLL.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace form
+{
+    internal class DadosBLL
+    {
+        public static void validaDados(Dados dados, char op)
+        {
+            //VALIDAÇÃO DE DADOS OBRIGATÓRIOS
+            Erro.setErro(false);
+            if (dados.getCodigo().Equals(""))
+            {
+                Erro.setMsg("O código é de preenchimento obrigatório!");
+                return;
+            }
+            if (dados.getDado1().Equals(""))
+            {
+                Erro.setMsg("O Dado1 é de preenchimento obrigatório!");
+                return;
+            }
+            if (dados.getDado2().Equals(""))
+            {
+                Erro.setMsg("O Dado2 é de preenchimento obrigatório!");
+                return;
+            }
+            if (dados.getDado3().Equals(""))
+            {
+                Erro.setMsg("A Dado3 é de preenchimento obrigatório!");
+                return;
+            }
+            if (dados.getDado4().Equals(""))
+            {
+                Erro.setMsg("O Dado4 é de preenchimento obrigatório!");
+                return;
+            }
+
+            //VALIDAÇÃO DE DADOS NUMÉRICOS E POSITIVOS (MAIOR QUE ZERO)
+            try
+            {
+                int.Parse(dados.getDado3());
+            }
+            catch (Exception)
+            {
+                Erro.setMsg("O valor do Dado3 deve ser numérico!");
+                return;
+            }
+
+            if (int.Parse(dados.getDado3()) <= 0)
+            {
+                Erro.setMsg("O valor do Dado3 deve ser numérico e positivo!");
+                return;
+            }
+
+            //VALIDAÇÃO DE DADOS NUMÉRICOS E POSITIVOS (MAIOR QUE ZERO)
+            try
+            {
+                int.Parse(dados.getDado4());
+            }
+            catch (Exception)
+            {
+                Erro.setMsg("O valor do Dado4 deve ser numérico!");
+                return;
+            }
+            if (float.Parse(dados.getDado4()) <= 0)
+            {
+                Erro.setMsg("O valor do Dado4 deve ser numérico e positivo!");
+                return;
+            }
+        }
+    }
+}
--- form/Form1.cs
+++ form/Form1.cs
@@ -1,3 +1,4 @@
+
 namespace form
 {
     public partial class Form1 : Form
@@ -17,6 +18,9 @@ namespace form
 
         private void button2_Click(object sender, EventArgs e)
         {
+            string salvar = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source="+Application.StartupPath+@"\Database1.accdb";
+            string comando = "INSERT INTO Database1(Dado1,Dado2,Dado3,Dado4)values(@Dado1,@Dado2,@Dado3,@Dado4)";
+
             teste.setCodigo(textBox5.Text);
             teste.setDado1(textBox4.Text);
             teste.setDado2(textBox1.Text);
